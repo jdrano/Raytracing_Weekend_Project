3468eaa6cdc86a7765724e93b0905af707d282ce
jdrano <jcdrano@gmail.com>
2023-11-24 23:31:37 -0800

    Add project files.

---
+++ Raytracing_Weekend/Raytracing_Weekend.cpp
@@ -0,0 +1,32 @@
+﻿// Raytracing_Weekend.cpp : Defines the entry point for the application.
+//
+
+#include "Raytracing_Weekend.h"
+#include "color.h"
+#include "vec3.h"
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Image
+    int image_width = 256;
+    int image_height = 256;
+
+    // Render
+
+    cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+    for (int j = 0; j < image_height; j++) {
+
+        // Adds a progress indicator
+        clog << "\rScanlines remaining: " << (image_height - j) << ' ' << flush;
+
+        for (int i = 0; i < image_width; i++) {
+            auto pixel_color = color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0);
+            write_color(std::cout, pixel_color);
+        }
+    }
+
+    clog << "\rDone.                 \n";
+}
---
+++ Raytracing_Weekend/Raytracing_Weekend.h
@@ -0,0 +1,8 @@
+﻿// Raytracing_Weekend.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
